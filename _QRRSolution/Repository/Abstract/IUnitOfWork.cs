d03927eed58c9d42c661b1e56476bfdafc9d4c77
Hannibal <38343963+hannibalt@users.noreply.github.com>
2020-02-21 10:24:35 +0300

    Solution Create/Layers Create
    Solution Create/Layers Create

---
+++ _QRRSolution/Repository/Abstract/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Abstract
+{
+
+    
+        public interface IUnitOfWork : IDisposable
+        {
+            IRepository<T> GetRepository<T>() where T : class;
+            int SaveChanges();
+        }
+    
+}
